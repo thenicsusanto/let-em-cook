9dd6e7a78966d881323255704b79f759377a2393
Abhinav-Ar <88677626+Abhinav-Ar@users.noreply.github.com>
2023-07-28 16:31:22 -0400

    commit!

---
+++ Assets/Scripts/burgerEnemyAI.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Oculus.Platform.Models;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class burgerEnemyAI : MonoBehaviour
+{
+
+    public GameObject player;
+    public Animator anim;
+
+    int health = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.SetBool("Coming Closer", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(player.transform.position.x - transform.position.x) > 5 &&
+            Mathf.Abs(player.transform.position.z - transform.position.z) > 5)
+        {
+            anim.SetBool("Attacking", true);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, player.transform.position, Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("fist"))
+        {
+            health -= 3;
+            if (health < 1)
+                Destroy(this.gameObject);
+        }
+
+        else if(collision.gameObject.CompareTag("Player"))
+        {
+            player.GetComponent<Movement>().health -= 10;
+
+        }
+    }
+}
