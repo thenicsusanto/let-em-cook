0c70055e0af81bfb5f0e34ea0190b4b1dbabfbbc
Abhinav-Ar <88677626+Abhinav-Ar@users.noreply.github.com>
2023-07-29 18:04:37 -0400

    Update GameLogic.cs

--- Assets/Settings/GameLogic.cs
+++ Assets/Settings/GameLogic.cs
@@ -16,6 +16,8 @@ public class GameLogic : MonoBehaviour
 
     int minute;
 
+    int enemiesSpawned;
+
 
     //game logic
     int[] numOfCustomers = { 5, 7, 10, 14, 18 };
@@ -25,26 +27,31 @@ public class GameLogic : MonoBehaviour
     int[] numOfEnemies = { 30, 33, 37, 48, 50 };
 
     //optimize later
-    int[][] round1 = {new int[] {7, 10}, new int[] { 5, 7}, new int[] { 2, 5} };
-    int[][] round2 = { new int[] { 6, 9 }, new int[] { 4, 6 }, new int[] { 2, 3 } };
-    int[][] round3 = { new int[] { 5, 8 }, new int[] { 3, 5 }, new int[] { 1, 3 } };
-    int[][] round4 = { new int[] { 4, 7 }, new int[] { 1, 3 }, new int[] { 1, 3 } };
-    int[][] round5 = { new int[] { 3, 6 }, new int[] { 1, 3 }, new int[] { 1, 3 } };
+    int[][] enemy1 = {new int[] {7, 10}, new int[] { 5, 7}, new int[] { 2, 5} };
+    int[][] enemy2 = { new int[] { 6, 9 }, new int[] { 4, 6 }, new int[] { 2, 3 } };
+    int[][] enemy3 = { new int[] { 5, 8 }, new int[] { 3, 5 }, new int[] { 1, 3 } };
+    int[][] enemy4 = { new int[] { 4, 7 }, new int[] { 1, 3 }, new int[] { 1, 3 } };
+    int[][] enemy5 = { new int[] { 3, 6 }, new int[] { 1, 3 }, new int[] { 1, 3 } };
 
 
     List<int[][]> rounds;
+
+    //game objects
+    public GameObject player;
+
 
     // Start is called before the first frame update
     void Start()
     {
         customersHelped = 0;
         customersFailed = 0;
+        enemiesSpawned = 0;
 
-        rounds.Add(round1);
-        rounds.Add(round2);
-        rounds.Add(round3);
-        rounds.Add(round4);
-        rounds.Add(round5);
+        rounds.Add(enemy1);
+        rounds.Add(enemy2);
+        rounds.Add(enemy3);
+        rounds.Add(enemy4);
+        rounds.Add(enemy5);
 
         minute = 0;
 
@@ -62,10 +69,27 @@ public class GameLogic : MonoBehaviour
         else
             minute = 0;
 
-        if(enemySpawn)
+        if(enemySpawn && enemiesSpawned < numOfEnemies[roundNum])
         {
             //instantiate later
+            StartCoroutine(nextEnemySpawn());
         }
+
+        if(customersHelped == numOfCustomers[roundNum])
+        {
+            //reload screen, but change roundNum to ++
+        }
+
+        if(customersFailed <= 3) {
+            //load failed screen
+        }
+
+        if(player.GetComponent<Movement>().health <= 0)
+        {
+            //load failed screen
+        }
+
+        
     }
 
     IEnumerator nextEnemySpawn()
