d88cb226cf4379fa3bf06947ec16ebd0e3261e6d
Abhinav-Ar <88677626+Abhinav-Ar@users.noreply.github.com>
2023-08-01 11:19:09 -0400

    dog

---
+++ Assets/Scripts/DogAttack.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogAttack : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = (player.transform.position - transform.position) * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(player.transform.position.x - transform.position.x) < 0.5 ||
+            Mathf.Abs(player.transform.position.y - transform.position.y) < 0.5)
+        {
+            player.GetComponent<Movement>().health -= 5;
+            Destroy(this.gameObject);
+        }
+
+        if (Time.time > 7)
+            Destroy(this.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
